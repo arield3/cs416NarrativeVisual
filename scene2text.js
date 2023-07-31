0b2af2d79f0be943ca90a1340d4d857a436cc6f8
dangw <wenxind3@illinois.edu>
2023-07-31 11:18:29 -0500

    update

--- scene2text.js
+++ scene2text.js
@@ -4,3 +4,17 @@ document.getElementById('economic').addEventListener('mouseover', function() {
 document.getElementById('economic').addEventListener('mouseout', function() {
     document.getElementById('economic-explain').style.display = 'none';
 });
+
+document.getElementById('Healthcare').addEventListener('mouseover', function() {
+    document.getElementById('Healthcare-explain').style.display = 'block';
+});
+document.getElementById('Healthcare').addEventListener('mouseout', function() {
+    document.getElementById('Healthcare-explain').style.display = 'none';
+});
+
+document.getElementById('Education').addEventListener('mouseover', function() {
+    document.getElementById('Education-explain').style.display = 'block';
+});
+document.getElementById('Education').addEventListener('mouseout', function() {
+    document.getElementById('Education-explain').style.display = 'none';
+});
